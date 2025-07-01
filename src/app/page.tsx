a844cc281a2aaa205e50eac608723de06a382b8d
the-speaker3 <the_speaker3@protonmail.com>
2025-07-01 18:59:12 +0200

    docs: Comment update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default function Home(): React.ReactNode {
     // Home Start
     <section className="home bg-primary flex-1">
       <h6 className="home__title hidden">Home</h6>
+      {/* TODO: Animations must be put on a timeline (sequence) */}
       <AnimationTypedTitles />
       <AnimationTypedArticle content={['Lorem ipsum dolor sit amet.']} />
       {/* TODO: Decide if these two comps will be part of page or layout (use global state or not) */}
