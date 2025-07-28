da0cfa2f8c4085430db65033321bbb33fbc1a052
the-speaker3 <the_speaker3@protonmail.com>
2025-07-28 15:29:24 +0200

    test: Unit/Integration setup

---
+++ frontend/src/__tests__/index.test.ts
@@ -0,0 +1,5 @@
+// Frontend Unit/Integration Test
+describe('Frontend Unit/Integration Test', () => {
+  // Indexing tests to output a well-organized testing report
+  //require('./components/*.test');
+});
